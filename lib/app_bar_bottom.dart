f9dfc891cbc0489ecee2344378ae7ce16e045dd1
José Carlos <jcjosecarlos03@gmail.com>
2024-12-13 07:47:42 -0300

    change layout

--- lib/app_bar_bottom.dart
+++ lib/app_bar_bottom.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vim_game/objective_score_mixin.dart';
 import 'package:vim_game/providers/providers.dart';
+import 'package:vim_game/theme/utils.dart';
 
 class ScoreAndInstructions extends ConsumerStatefulWidget
     implements PreferredSizeWidget {
@@ -26,31 +27,88 @@ class _ScoreAndInstructionsState extends ConsumerState<ScoreAndInstructions>
   @override
   Widget build(BuildContext context) {
     ref.listen(carretProvider, onCarretPositionChange);
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        _Instructions(),
-        _Score(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _Instructions(),
+          _Score(),
+        ],
+      ),
     );
   }
 }
 
 class _Instructions extends StatelessWidget {
-  const _Instructions({super.key});
+  const _Instructions();
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Go to the X');
+    return Row(
+      children: [
+        Text(
+          'Goal:',
+          style: context.bodyMedium.copyWith(
+            fontWeight: FontWeight.w600,
+            color: context.primary,
+          ),
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: context.primary,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Text(
+              'Go to the X',
+              style: context.bodyMedium.copyWith(
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+                color: context.onPrimary,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
 class _Score extends ConsumerWidget {
-  const _Score({super.key});
+  const _Score();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final scoreCount = ref.watch(scoreProvider);
-    return Text('Score: $scoreCount');
+    return Row(
+      children: [
+        Text(
+          'Score: ',
+          style: context.bodyMedium.copyWith(
+            fontWeight: FontWeight.w600,
+            color: context.primary,
+          ),
+        ),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            color: context.primary,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              '$scoreCount',
+              style: context.bodyMedium.copyWith(
+                fontWeight: FontWeight.w600,
+                color: context.onPrimary,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
